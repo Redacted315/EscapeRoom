1d4a0d839ba496696ea4b635331319fdc85f5f91
Matthew <matoli315@gmail.com>
2025-03-05 13:27:26 -0700

    Update game.py

--- game.py
+++ game.py
@@ -1,10 +1,17 @@
 import tkinter as tk
 from tkinter.font import BOLD, Font
 from datetime import datetime, timedelta
+from pygame import mixer  # Used for audio playback.
 
 class Game():
 
     def __init__(self) -> None:
+        hint_file = ["assets\\audio\\hint_1.mp3", "assets\\audio\\hint_2.mp3", "assets\\audio\\hint_3.mp3", "assets\\audio\\hint_4.mp3"]
+        mixer.init()
+        self.hints = []
+        # self.sound_wrong = mixer.Sound("assets\\audio\\wrong.mp3")
+        for hint in hint_file:
+            self.hints.append(mixer.Sound(hint))
         self.root = tk.Tk()
         self.screen_width = self.root.winfo_screenwidth()
         self.screen_height = self.root.winfo_screenheight()
@@ -13,7 +20,7 @@ class Game():
         self.timer_frame = tk.Frame(self.root)
         self.timer_frame.place(x=5, y=0)
         self.start_time = datetime.now()
-        self.end_time = self.start_time + timedelta(minutes=30) # length of game timer
+        self.end_time = self.start_time + timedelta(minutes=1) # length of game timer
         # self.counter = 10
         self.timer_label = tk.Label(self.timer_frame, text=f"", font=self.timer_font)
         self.timer_label.pack()
@@ -27,13 +34,25 @@ class Game():
         self.dash_2 = tk.Label(self.entry_frame, font=self.entry_font, text="-")
         self.entry_3 = tk.Entry(self.entry_frame,font=self.entry_font,  width=4)
 
-        self.entry_1.grid(column=0, row=0)
+        self.entry_1.grid(column=0, row=0, padx=75)
         self.dash_1.grid(column=1, row=0)
-        self.entry_2.grid(column=2, row=0)
+        self.entry_2.grid(column=2, row=0, padx=75)
         self.dash_2.grid(column=3, row=0)
-        self.entry_3.grid(column=4, row=0)
-        self.pos_x = ( self.screen_width / 2 ) - ( self.entry_frame.winfo_width() / 2 )-75
+        self.entry_3.grid(column=4, row=0, padx=75)
+        self.pos_x = ( self.screen_width / 2 ) - ( self.entry_frame.winfo_width() / 2 )
         self.entry_frame.place(x=self.pos_x, y=self.screen_height/2)
+
+        self.hint_frame = tk.Frame(self.root)
+        self.hint_1 = tk.Button(self.hint_frame, text="🔉", command=lambda: self.hints[0].play())
+        self.hint_2 = tk.Button(self.hint_frame, text="🔉", command=lambda: self.hints[1].play())
+        self.hint_3 = tk.Button(self.hint_frame, text="🔉", command=lambda: self.hints[2].play())
+        # self.hint_4 = tk.Button(self.hint_frame, text="🔉", command=lambda: self.hints[3].play())
+        self.hint_1.grid(column=0, row=0, padx=75)
+        self.hint_2.grid(column=1, row=0, padx=75)
+        self.hint_3.grid(column=2, row=0, padx=75)
+        # self.hint_4.grid(column=3, row=0, padx=75)
+        self.hint_frame.place(x=self.pos_x, y=(self.screen_height/2)+50)
+        self.submit_btn = tk.Button(self.root, text="Submit", command=self.submit).place(x=self.pos_x, y=(self.screen_height/2)+100)
 
         # -------------------- for testing --------------------
         self.root.bind("<F11>", self.toggle_fullscreen) #     |
@@ -58,10 +77,16 @@ class Game():
             self.root.after(1000, update, 0)
 
         self.root.after(0, update, 0)
+    
+    def submit(self):
+        entered_pswd = f"{self.entry_1.get()}{self.entry_2.get()}{self.entry_3.get()}"
+        print(entered_pswd)
+
 
 
 def test():
     w = Game()
+
 
 if __name__ == '__main__':
     test()
